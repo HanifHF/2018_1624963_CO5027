932082e092207cdf92640e58ed49a988b73ca070
HanifHF <anyf422@outlook.com>
2018-04-09 14:43:35 +0800

    Fixing codes2

--- 1624963_CO5027/register.aspx.cs
+++ 1624963_CO5027/register.aspx.cs
@@ -76,7 +76,7 @@ namespace _1624963_CO5027
                 }
             }
         }
-
+        
         private void LogUserIn ( UserManager <IdentityUser> usermanager, IdentityUser user) 
         {
             var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
